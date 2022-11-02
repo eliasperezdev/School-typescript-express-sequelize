64d2a34a74f97385af331ad99a188e6ee10b5fa9
elias <perexlito@gmail.com>
2022-11-02 16:37:09 -0300

    add index models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,13 @@
 import { Sequelize, DataTypes, Options } from 'sequelize';
 import initUserModel from './User';
 import initRoleModel from './Role';
+import initClassModel from './Class';
+import initAttendaceModel from './Attendace';
+import initParentModel from './Parent';
+import initPeriodModel from './Period';
+import initStudentModel from './Student';
+import initSubjectModel from './Subject';
+import initTeacherModel from './Teacher';
 import configFile from '../db/config/config';
 
 type EnvType = 'development' | 'test' | 'production';
@@ -11,10 +18,24 @@ const sequelize = new Sequelize(config);
 const db = {
   User: initUserModel(sequelize, DataTypes),
   Role: initRoleModel(sequelize, DataTypes),
+  Class: initClassModel(sequelize, DataTypes),
+  Attendace: initAttendaceModel(sequelize, DataTypes),
+  Parent: initParentModel(sequelize, DataTypes),
+  Student: initStudentModel(sequelize, DataTypes),
+  Period: initPeriodModel(sequelize, DataTypes),
+  Subject: initSubjectModel(sequelize, DataTypes),
+  Teacher: initTeacherModel(sequelize, DataTypes),
   sequelize,
 };
 
 db.Role.associate(db);
 db.User.associate(db);
+db.Class.associate(db);
+db.Attendace.associate(db);
+db.Parent.associate(db);
+db.Student.associate(db);
+db.Period.associate(db);
+db.Subject.associate(db);
+db.Teacher.associate(db);
 
 export default db;
