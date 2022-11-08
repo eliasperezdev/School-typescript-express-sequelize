8a11c419899c9dd9524133a98c1e730dce8b53b7
elias <perexlito@gmail.com>
2022-11-08 17:00:28 -0300

    add migration student

---
+++ src/db/migrations/1-create-Student.ts
@@ -0,0 +1,43 @@
+module.exports = {
+  async up(queryInterface:any, Sequelize:any) {
+    await queryInterface.createTable('students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      genrer: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface:any, Sequelize:any) {
+    await queryInterface.dropTable('Users');
+  },
+};
