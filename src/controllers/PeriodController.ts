c85547a264eef178a4052100be82829ad5a888d8
elias <perexlito@gmail.com>
2022-11-08 22:07:13 -0300

    add controller update period

--- src/controllers/PeriodController.ts
+++ src/controllers/PeriodController.ts
@@ -14,12 +14,29 @@ const getPeriods = async (req: Request, res: Response) => {
   }
 };
 
-const getPeriod = async (req: Request, res: Response) => {
-  console.log('get Period');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getPeriod = async (_req: Request, _res: Response) => {
+  // TODO ver que devolver Materias? Clases?
 };
 
 const updatePeriod = async (req: Request, res: Response) => {
-  console.log('update Period');
+  const { id } = req.params;
+  const {
+    startDate, endDate
+  } = req.body;
+  try {
+    const period = await db.Period.findOne({ where: { id } });
+    if (!period) {
+      return res.status(404).json('No found');
+    }
+    if (startDate) period.startDate = startDate;
+    if (endDate) period.endDate = endDate;
+
+    period.save();
+    return res.status(200).json(period);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
 
 export default {
