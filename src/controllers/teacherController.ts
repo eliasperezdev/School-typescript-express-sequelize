5662adfb24273529becf3110e818a5e12e0de72c
elias <perexlito@gmail.com>
2022-11-08 21:49:32 -0300

    add controller update teacher

--- src/controllers/teacherController.ts
+++ src/controllers/teacherController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 import db from '../models';
+import { Teacher } from '../models/Teacher';
 
 const addTeacher = async (req: Request, res: Response) => {
-  const { firstName, lastName, phone, email } = req.body;
+  const {
+    firstName, lastName, phone, email,
+  } = req.body;
 
   try {
     const newTeacher = await db.Teacher.create({
@@ -26,7 +29,25 @@ const getTeacher = async (req: Request, res: Response) => {
 };
 
 const updateTeacher = async (req: Request, res: Response) => {
-  console.log('update Teacher');
+  const { id } = req.params;
+  const {
+    firstName, lastName, phone, email,
+  } = req.body;
+  try {
+    const teacher = await db.Teacher.findOne({ where: { id }});
+    if (!teacher) {
+      return res.status(404).json('No found');
+    }
+    if (firstName) teacher.firstName = firstName;
+    if (lastName) teacher.lastName = lastName;
+    if (phone) teacher.phone = phone;
+    if (email) teacher.email = email;
+
+    teacher.save();
+    return res.status(200).json(teacher);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
 
 const deleteTeacher = async (req: Request, res: Response) => {
